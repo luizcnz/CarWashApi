c32b957baab043a95ac8d17de083c68630ea942b
Luiz <cluisromero@hotmail.com>
2021-08-06 01:06:49 -0600

    correccion del controlador del tipo de vehiculos y el modelo de Modelo de vehiculos

--- src/models/vehicles/models.php
+++ src/models/vehicles/models.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dany_Hernandez
- * Date: 13/7/2021
- * Time: 16:43
- */
+
+namespace Api\models\vehicles;
+
+class Models
+{
+    public $idModeloVehiculos;
+    public $modelo;
+
+}
+
+?>
