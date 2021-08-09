cd8f18096973b1d4af434adee2ad9bd95e3ad4e7
dmhernanandez <dany1999hernan@gmail.com>
2021-08-09 12:48:22 -0600

    Testeo de rutas

--- src/utils/UploadFile.php
+++ src/utils/UploadFile.php
@@ -29,7 +29,7 @@ class UploadFile
         $url=$defaultImage;
         $type=$defaultImage==Constants::IMG_USER_DEFAULT?"user":"car";//Valida donde va guardar
         $directory=__DIR__."/../../".$directory."/".$type;
-
+       echo $directory;
         $uploadedFile = $uploadedFiles[Constants::IMG_UPLOAD_NAME];
         if ($this->isFileUploaded($uploadedFile))
         {
